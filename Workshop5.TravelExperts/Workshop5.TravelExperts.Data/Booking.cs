5151213f66dbef6b0e2f530a5fee45650279d914
Tim Leslie <tdleslie1@gmail.com>
2019-04-05 17:52:35 -0600

    OrderSummary changes TL.

---
+++ Workshop5.TravelExperts/Workshop5.TravelExperts.Data/Booking.cs
@@ -0,0 +1,31 @@
+﻿/********************************************************************************
+* 
+* Author: Tim Leslie
+* Date: April 5, 2019.
+* Course: CPRG 217 Rapid OOSD Threaded Project
+* Assignment: Workshop 5
+* Purpose: This is a Booking class definition and forms part of the CPRG 214
+* Threaded Project Workshop 5.
+*
+*********************************************************************************/
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop5.TravelExperts.Data
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public DateTime? BookingDate { get; set; }
+        public string BookingNo { get; set; }
+        public double? TravelerCount { get; set; }
+        public int? CustomerId { get; set; }
+        public string TripTypeId { get; set; }
+        public int? PackageId { get; set; }
+
+    }
+}
---
+++ Workshop5.TravelExperts/Workshop5.TravelExperts.Domain/BookingDetailDB.cs
@@ -0,0 +1,101 @@
+﻿/********************************************************************************
+* 
+* Author: Tim Leslie
+* Date: April 5, 2019.
+* Course: CPRG 217 Rapid OOSD Threaded Project
+* Assignment: Workshop 5
+* Purpose: This is a Booking class definition and forms part of the CPRG 214
+* Threaded Project Workshop 5.
+*
+*********************************************************************************/using System;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workshop5.TravelExperts.Data;
+
+namespace Workshop5.TravelExperts.Domain
+{
+    public class BookingDetailDB
+    {
+        public static List<BookingDetail> GetAllBookingDetails()
+        {
+            List<BookingDetail> bookingDetails = new List<BookingDetail>(); // instantiate an empty List of Bookings
+
+            SqlConnection conn = TravelExpertsDB.GetConnection(); // instantiate a DB connection object
+
+            // prepare the SQL statement
+            string selectStatement = "SELECT * FROM BookingDetails";
+
+            SqlCommand selectCommand = new SqlCommand(selectStatement, conn);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader dr = selectCommand.ExecuteReader();
+                while (dr.Read())
+                {
+                    BookingDetail bookingDetail= new BookingDetail();
+                    bookingDetail.BookingDetailId = (int)dr["BookingDetailId"];
+
+                    if (dr["ItineraryNo"] == DBNull.Value)
+                        bookingDetail.ItineraryNo = null;
+                    else
+                        bookingDetail.ItineraryNo = (double)dr["ItineraryNo"];
+
+                    if (dr["TripStart"] == DBNull.Value)
+                        bookingDetail.TripStart = null;
+                    else
+                        bookingDetail.TripStart = (DateTime)dr["TripStart"];
+
+                    if (dr["TripEnd"] == DBNull.Value)
+                        bookingDetail.TripStart = null;
+                    else
+                        bookingDetail.TripStart = (DateTime)dr["TripEnd"];
+
+                    bookingDetail.Description = (string)dr["Description"];
+
+                    bookingDetail.Destination = (string)dr["Destination"];
+
+                    if (dr["BasePrice"] == DBNull.Value)
+                        bookingDetail.BasePrice = null;
+                    else
+                        bookingDetail.BasePrice = (decimal)dr["BasePrice"];
+
+                    if (dr["AgencyCommission"] == DBNull.Value)
+                        bookingDetail.AgencyCommission = null;
+                    else
+                        bookingDetail.AgencyCommission = (decimal)dr["AgencyCommission"];
+
+                    if (dr["BookingId"] == DBNull.Value)
+                        bookingDetail.BookingId = null;
+                    else
+                        bookingDetail.BookingId = (int)dr["BookingId"];
+
+
+                    bookingDetail.RegionId = (string)dr["RegionId"];
+                    bookingDetail.ClassId = (string)dr["ClassId"];
+                    bookingDetail.FeeId = (string)dr["FeeId"];
+
+                    if (dr["ProductSupplierId"] == DBNull.Value)
+                        bookingDetail.ProductSupplierId= null;
+                    else
+                        bookingDetail.ProductSupplierId = (int)dr["ProductSupplierId"];
+
+                    bookingDetails.Add(bookingDetail);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return bookingDetails;
+        }
+    }
+}
