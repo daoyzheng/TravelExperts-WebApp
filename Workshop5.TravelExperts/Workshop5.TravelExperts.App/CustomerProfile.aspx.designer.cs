5c67ba3e1a510cb92b75d654a0b8d353f58b7229
mahda <mahda628@yahoo.com>
2019-04-08 21:24:10 -0600

    add province validation for edit

--- Workshop5.TravelExperts/Workshop5.TravelExperts.App/CustomerProfile.aspx.designer.cs
+++ Workshop5.TravelExperts/Workshop5.TravelExperts.App/CustomerProfile.aspx.designer.cs
@@ -157,6 +157,15 @@ namespace Workshop5.TravelExperts.App {
         protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
         
         /// <summary>
+        /// RequiredFieldValidator12 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator12;
+        
+        /// <summary>
         /// txtPostal control.
         /// </summary>
         /// <remarks>
